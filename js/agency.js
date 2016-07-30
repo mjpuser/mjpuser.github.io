da22814b2000a0ba0d8886c4b2b51ba336b05dad
Matthew Prestifilippo <m.prestifilippo@gmail.com>
2016-07-30 16:24:46 -0400

    Fixed contact me form

--- js/agency.js
+++ js/agency.js
@@ -4,7 +4,7 @@
  *
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
- * 
+ *
  * Copyright 2013, Codrops
  * http://www.codrops.com
  */
@@ -68,4 +68,64 @@ $('body').scrollspy({
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
+});
+
+
+
+$(function() {
+	$("input,textarea").jqBootstrapValidation({
+		preventSubmit: true,
+		submitError: function($form, event, errors) {
+			// additional error messages or events
+		},
+		submitSuccess: function($form, event) {
+			event.preventDefault(); // prevent default submit behaviour
+			// get values from FORM
+			var name = $("input#name").val();
+			var email = $("input#email").val();
+			var phone = $("input#phone").val();
+			var message = $("textarea#message").val();
+			var firstName = name; // For Success/Failure Message
+			// Check for white space in name for Success/Fail message
+			if (firstName.indexOf(' ') >= 0) {
+				firstName = name.split(' ').slice(0, -1).join(' ');
+			}
+			var script = document.createElement('script');
+			script.src = [
+				'https://script.google.com/macros/s/AKfycbytecLNLWJxs4As7h5-ZGUkd1aFJinuhL-5ZspEInpxinowgqr5/exec?',
+				[
+					['name', name].join('='),
+					['phone', phone].join('='),
+					['email', email].join('='),
+					['message', message].join('=')
+				].join('&')
+			].join('');
+			document.body.appendChild(script);
+			// Success message
+			$('#success').html("<div class='alert alert-success'>");
+			$('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+				.append("</button>");
+			$('#success > .alert-success')
+				.append("<strong>Your message has been sent. </strong>");
+			$('#success > .alert-success')
+				.append('</div>');
+
+			//clear all fields
+			$('#contactForm').trigger("reset");
+		},
+		filter: function() {
+			return $(this).is(":visible");
+		}
+	});
+
+	$("a[data-toggle=\"tab\"]").click(function(e) {
+		e.preventDefault();
+		$(this).tab("show");
+	});
+});
+
+
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function() {
+    $('#success').html('');
 });
